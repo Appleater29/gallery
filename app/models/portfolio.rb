2c1b3e11c1e256794ffaabbe045f24b26f410171
Matthew Oates <mattysamueloates@gmail.com>
2025-08-29 17:28:33 +0100

    Thumbnails link to images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,10 @@ class Portfolio < ApplicationRecord
     has_many_attached :images
     has_rich_text :description
     validates :name, presence: true
+
+    def thumbnail_images
+        images.map do |image|
+            image.variant(resize_to_limit: [300, 300])
+        end
+    end
 end
